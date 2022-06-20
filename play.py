9def29a9e3a9c909f0769b3fe5bcce988f5cb0d6
Damon-X46 <956191114@qq.com>
2022-06-20 19:41:51 +0800

    20220620

--- play.py
+++ play.py
@@ -1,30 +1,35 @@
 class Solution:
-    def letterCasePermutation(self, s):
+    def merge(self, nums1, m: int, nums2, n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(len(nums2)):
+            nums1[m + i] = nums2[i]
+        # nums1.sort()
+        nums1 = self.quick_sort(nums1, 0, len(nums1) - 1)
+    
+    def quick_sort(self, nums, head, tail):
+        if head >= tail:
+            return
+        pivot = nums[head]
+        left = head
+        right = tail
+        while left != right:
+            while left < right and nums[right] > pivot:
+                right -= 1
+            nums[left] = nums[right]
+            while left < right and nums[left] < pivot:
+                left += 1
+            nums[right] = nums[left]
+        nums[left] = pivot
+        self.quick_sort(nums, head, left - 1)
+        self.quick_sort(nums, left + 1, tail)
+        return nums
 
-        path = []
-        result = []
-
-        def backtracking(s, startIndex):
-            if len(path) == len(s):
-                result.append("".join(path[:]))
-                return
-            
-            c = s[startIndex]
-            if c.isdigit():
-                path.append(c)
-                backtracking(s, startIndex + 1)
-                path.pop()
-            else:
-                path.append(c.lower())
-                backtracking(s, startIndex + 1)
-                path.pop()
-
-                path.append(c.upper())
-                backtracking(s, startIndex + 1)
-                path.pop()
-        backtracking(s, 0)
-        return result
 
 aaa = Solution()
-bbb = aaa.letterCasePermutation("a1b2")
-print(bbb)
+bbb = aaa.merge([1,2,3,0,0,0],3,[2,5,6],3)
+
+nums = [1,2,2,3,6,4,4,4,5]
+bbb = aaa.quick_sort(nums)
+pass
