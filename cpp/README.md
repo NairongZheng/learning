5457e68ff5bc2de924df9ad407aded40649b4030
Damon-X46 <956191114@qq.com>
2022-11-02 21:50:24 +0800

    20221102

--- cpp/README.md
+++ cpp/README.md
@@ -1,5 +1,6 @@
 
 # cpp这是c++版本
-1. array-binary_search-binary_search_1：vector的定义及传参
-2. array-binary_search-binary_search_3：二分查找的mid怎么定义
-3. array-binary_search-binary_search_4：c++数组下标不能有负数
+1. array-binary_search-binary_search1：vector的定义及传参
+2. array-binary_search-binary_search3：二分查找的mid怎么定义
+3. array-binary_search-binary_search4：c++数组下标不能有负数
+4. array-sliding_window-sliding_window0：c++无穷大无穷小怎么表示，set的使用
---
+++ cpp/array/sliding_window/sliding_window0_lc3.cpp
@@ -0,0 +1,43 @@
+/*
+    无重复字符的最长子串
+    给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+*/
+
+#include <iostream>
+#include <limits.h>
+#include <set>
+using namespace std;
+
+int lengthOfLongestSubstring(string s)
+{
+    int start = 0;
+    int max_len = INT_MIN; // int无穷大用INT_MAX表示，无穷小用INT_MIN表示，要包含头文件limits.h
+    set<int> mark;
+    for (int end = 0; end < s.size(); end++)
+    {
+        while (mark.find(s[end]) != mark.end()) // 元素是否在集合中
+        {
+            mark.erase(s[start]);   // 删除集合中的元素
+            start++;
+        }
+        max_len = max(max_len, end - start + 1);
+        mark.insert(s[end]);    // 往集合中添加元素
+    }
+    if (max_len == INT_MIN)
+    {
+        return 0;
+    }
+    else
+    {
+        return max_len;
+    }
+}
+
+int main()
+{
+    string s = "abcabcbb";
+    int target = 0;
+    int result = lengthOfLongestSubstring(s);
+    cout << result << endl; // 3
+    return 0;
+}
