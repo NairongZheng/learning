55c3cc2c7495c0148d309b0e58e2a4f78d5e9c7d
NairongZheng <956191114@qq.com>
2024-05-30 22:03:34 +0800

    [zheng] add fastapi to http connection (20240530)

---
+++ connection/http_connect/fastapi_base/client.py
@@ -0,0 +1,34 @@
+import requests
+
+def test_sum():
+    request = {"para_a": 10, "para_b": 36}
+    print(f"debug damonzheng, request:{request}")
+    response = requests.post(url="http://localhost:12300/sumHandler", json=request)
+    if response.status_code == 200:
+        result = response.json()
+        if result["errcode"] == 200:
+            print("sum result from server:", result["data"])
+        elif result["errcode"] == 400:
+            print("Server error:", result["data"])
+    else:
+        print("Failed to connect to server")
+
+def test_upper():
+    request = "lower string to upper string"
+    print(f"debug damonzheng, request:{request}")
+    response = requests.post(url="http://localhost:12300/upperHandler", data=request)
+    if response.status_code == 200:
+        result = response.json()
+        if result["errcode"] == 200:
+            print("Uppercase string from server:", result["data"])
+        elif result["errcode"] == 400:
+            print("Server error:", result["data"])
+    else:
+        print("Failed to connect to server")
+
+def main():
+    test_sum()
+    test_upper()
+
+if __name__ == "__main__":
+    main()
