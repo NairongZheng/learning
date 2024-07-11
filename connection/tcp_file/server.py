03e2185bf56d99c45b50b2bd3365453081f8cdad
NairongZheng <956191114@qq.com>
2024-07-11 19:23:43 +0800

    [zheng] update tcp_file (20240711)

--- connection/tcp_file/server.py
+++ connection/tcp_file/server.py
@@ -8,28 +8,44 @@ class Server:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(5)
+    
+    def send_data(self, client_socket, send_msg: Message):
+        serialized_data = send_msg.SerializeToString()
+        size_bytes = len(serialized_data).to_bytes(4, 'big')
+        with client_socket.makefile('wb') as file:
+            file.write(size_bytes)
+            file.write(serialized_data)
+
+    def receive_data(self, client_socket):
+        with client_socket.makefile('rb') as file:
+            size_bytes = file.read(4)
+            size = int.from_bytes(size_bytes, 'big')
+            recv_data = file.read(size)
+            recv_msg = Message()
+            recv_msg.ParseFromString(recv_data)
+        return recv_msg
+    
+    def process_data(self, msg: Message):
+        content = msg.content               # 收到的请求数据的实际内容
+        send_content = content.upper()      # 处理
+        send_msg = Message()
+        send_msg.content = send_content
+        return send_msg
 
     def run(self):
-        print(f"Server listening on {self.host}:{self.port}")
+        print(f"server listening on {self.host}:{self.port}")
         while True:
             client_socket, address = self.server_socket.accept()
-            print(f"Connection from {address}")
-            with client_socket.makefile('rb') as file:
-                size_bytes = file.read(4)
-                size = int.from_bytes(size_bytes, 'big')
-                received_data = file.read(size)
-                if received_data:
-                    msg = Message()
-                    msg.ParseFromString(received_data)
-                    print("Received(len):", len(msg.content))
-                    response_msg = self.process_data(msg)
-                    response_data = response_msg.SerializeToString()
-                    size_bytes = len(response_data).to_bytes(4, 'big')
-                    client_socket.sendall(size_bytes + response_data)
+            print(f"connection from {address}")
+            recv_msg = self.receive_data(client_socket)
+            # print("recv_data:", recv_msg.content)
+            print("received(len):", len(recv_msg.content))
+            send_msg = self.process_data(recv_msg)
+            # print("send_data:", send_msg.content)
+            print("send(len):", len(send_msg.content))
+            self.send_data(client_socket, send_msg)
             client_socket.close()
-
-    def process_data(self, msg):
-        return Message(content=msg.content.upper())
+    
 
 if __name__ == "__main__":
     server = Server('127.0.0.1', 8888)
