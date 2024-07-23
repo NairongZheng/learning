8d9d06eb50846010982b4a689d3808e65ead6c7b
NairongZheng <956191114@qq.com>
2024-07-23 12:00:53 +0800

    [zheng] add README of connection/http_connect/fastapi_base (20240723)

--- connection/http_connect/fastapi_base/server.py
+++ connection/http_connect/fastapi_base/server.py
@@ -1,4 +1,13 @@
+"""
+server
+可以用不同的方式写
+1. 自己定义请求跟返回的格式
+2. 直接用简单的Request跟JSONResponse
+第二种比较简单方便
+具体说明查看README
+"""
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel
 import uvicorn
 
@@ -21,23 +30,36 @@ class UpperResponse(BaseModel):
     data: str
     msg: str
 
+# 第一种写法
 @app.post("/sumHandler", response_model=SumResponse)
-async def sum_handler(request: SumRequest):
-    print(f"debug damonzheng, SumHandler_request:{request}")
+async def sum_handler(request: SumRequest, http_request: Request):
+    print(f"debug damonzheng, sumHandler接收来自{http_request.client.host}:{http_request.client.port}的请求")
+    print(f"debug damonzheng, sumHandler接收的请求内容是:{request}")
     try:
         res = request.para_a + request.para_b
         return SumResponse(errcode=200, data=res, msg="")
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@app.post("/upperHandler", response_model=UpperResponse)
-async def upper_handler(request: UpperRequest):
-    print(f"debug damonzheng, UpperHandler_request:{request}")
-    try:
-        upper_case_data = request.para_str.upper()
-        return UpperResponse(errcode=200, data=upper_case_data, msg="Successful parameter passing")
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+# upperHandler第二种写法，注意async跟await
+@app.post("/upperHandler")
+async def upper_handler(request: Request):
+    print(f"debug damonzheng, upperHandler接收来自{request.client.host}:{request.client.port}的请求")
+    request = await request.json()
+    print(f"debug damonzheng, upperHandler接收的请求内容是:{request}")
+    result = request.get("para_str").upper()
+    response = {"errcode": 200, "data": result, "msg": ""}
+    return JSONResponse(content=response)
+
+# upperHandler第一种写法
+# @app.post("/upperHandler", response_model=UpperResponse)
+# async def upper_handler(request: UpperRequest):
+#     print(f"debug damonzheng, UpperHandler_request:{request}")
+#     try:
+#         upper_case_data = request.para_str.upper()
+#         return UpperResponse(errcode=200, data=upper_case_data, msg="Successful parameter passing")
+#     except Exception as e:
+#         raise HTTPException(status_code=400, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=12300)
+    uvicorn.run(app="server:app", host="0.0.0.0", port=12300)
