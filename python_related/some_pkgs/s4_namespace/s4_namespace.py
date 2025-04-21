dc022051cd011c0118afee4142ace138ea43bcd9
NairongZheng <956191114@qq.com>
2025-04-21 14:28:14 +0800

    [zheng] 完善namespace (20250421)

--- python_related/some_pkgs/s4_namespace/s4_namespace.py
+++ python_related/some_pkgs/s4_namespace/s4_namespace.py
@@ -21,6 +21,7 @@ class FakeVars:
         variables.people_related = SimpleNamespace()
         variables.functions = SimpleNamespace()
         # general_vars
+        variables.general_vars.type = "class2namespace"
         variables.general_vars.version = "1.1.1"
         variables.general_vars.date = "20240919"
         # people_related
@@ -37,6 +38,7 @@ def dict2namespace():
     people_num = 2
     variables = {
         "general_vars": {
+            "type": "dict2namespace",
             "version": "1.1.1",
             "date": "20250418"
         },
@@ -53,18 +55,54 @@ def dict2namespace():
     return adict_namespace
 
 
-def main():
-    # 写类直接构建返回，比较麻烦
+def class2namespace():
     fake_vars = FakeVars()
-    variables_1_namespace = fake_vars.set_vars()
-    print(variables_1_namespace)
-    variables_1_dict = NamespaceUtils.namespace_to_dict(variables_1_namespace) # 命名空间转化成字典
-    print(variables_1_dict)
+    variables = fake_vars.set_vars()
+    return variables
+
+
+def json2namespace():
+    json_file_path = "python_related/some_pkgs/s4_namespace/tmp_files/json2namespace.json"
+    variables = NamespaceUtils.load_json(json_file_path)
+    return variables
+
+
+def yaml2namespace():
+    yaml_file_path = "python_related/some_pkgs/s4_namespace/tmp_files/yaml2namespace.yaml"
+    variables = NamespaceUtils.load_yaml(yaml_file_path)
+    return variables
+
+
+def namespace2json(ns):
+    save_file_path = "python_related/some_pkgs/s4_namespace/tmp_files/namespace2json.json"
+    NamespaceUtils.save_to_json(ns, save_file_path)
+
+
+def main():
+    # 1. 用类构建namespace（较麻烦）
+    print(f" ==================== class to namespace ==================== ")
+    variables_1_namespace = class2namespace()
+    print(f"class2namespace: {variables_1_namespace}\n")
     
-    # 用字典自动转换，很方便
+    # 2. 用字典构建namespace（方便点）
+    print(f" ==================== dict to namespace ==================== ")
     variables_2_namespace = dict2namespace()
-    # NamespaceUtils.pretty_print_namespace(variables_2_namespace)
-    pass
+    print(f"dict2namespace: {variables_2_namespace}\n")
+    
+    # 3. 用json构建namespace（JSON 只支持基础数据类型）
+    print(f" ==================== json to namespace ==================== ")
+    variables_3_namespace = json2namespace()
+    print(f"json2namespace: {variables_3_namespace}\n")
+    
+    # 4. 用yaml构建namespace
+    print(f" ==================== yaml to namespace ==================== ")
+    variables_4_namespace = yaml2namespace()
+    print(f"json2namespace: {variables_4_namespace}\n")
+    
+    # 5. namespace保存到json
+    print(f" ==================== namespace to json ==================== ")
+    namespace2json(variables_2_namespace)
+    print(f"save success")
 
 
 if __name__ == "__main__":
